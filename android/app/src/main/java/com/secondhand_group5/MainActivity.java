ee3475932f7934d4584a8b9e9935c4573ee23082
vandevee3 <garindrawardhana@gmail.com>
2022-06-18 20:42:12 +0700

    menambahkan bottom navigation

--- android/app/src/main/java/com/secondhand_group5/MainActivity.java
+++ android/app/src/main/java/com/secondhand_group5/MainActivity.java
@@ -4,12 +4,20 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected void onCreate(Bundle ReactActivity){
+    super.onCreate(null);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "SecondHand_Group5";
