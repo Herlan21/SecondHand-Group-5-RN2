48409e826fab03a117cf81ffda2c62eb08b1db94
Herlan <herlan.n9d@gmail.com>
2022-06-21 13:19:56 +0700

    fixing router and codepush

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Home, Notifikasi, Jual, DaftarJual, Akun, } from "../screens"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import { NavigationContainer } from "@react-navigation/native";
 import { purple } from "../constant";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Feather";
 
 
 const Stack = createNativeStackNavigator()
@@ -16,18 +15,30 @@ const MainApp = () => {
             tabBarActiveTintColor: purple
         }} >
 
-            <Tab.Screen name="Home" component={Home} options={{
-                headerShown: false,
-                TabBarIcon: ({ color }) => (
-                    <Icon name="home-outline" size={24} color={color} />
-                )
-            }}
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{ headerShown: false,
+                           tabBarIcon: ({ color }) => (<Icon name="home" color={color} size={24} />)}}
             />
-            
-            <Tab.Screen name="Notifikasi" component={Notifikasi} />
-            <Tab.Screen name="Jual" component={Jual} />
-            <Tab.Screen name="DaftarJual" component={DaftarJual} />
-            <Tab.Screen name="Akun" component={Akun} />
+
+            <Tab.Screen name="Notifikasi" component={Notifikasi} 
+                        options={{ headerShown: false,
+                                   tabBarIcon: ({ color }) => (<Icon name="bell" color={color} size={24} />)}}
+            />
+
+            <Tab.Screen name="Jual" component={Jual} 
+                        options={{ headerShown: false,
+                                   tabBarIcon: ({ color }) => (<Icon name="plus" color={color} size={24} />)}}
+            />
+            <Tab.Screen name="DaftarJual" component={DaftarJual} 
+                        options={{ headerShown: false,
+                                   tabBarIcon: ({ color }) => (<Icon name="list" color={color} size={24} />)}}
+            />
+            <Tab.Screen name="Akun" component={Akun} 
+                        options={{ headerShown: false,
+                                   tabBarIcon: ({ color }) => (<Icon name="user" color={color} size={24} />)}}
+            />
         </Tab.Navigator>
     )
 }
