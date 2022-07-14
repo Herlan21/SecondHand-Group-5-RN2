067eff82edf5e03026c6a63dd13b9e5ce9c422cd
Herlan <herlan.n9d@gmail.com>
2022-07-14 20:26:39 +0700

    get API data product to home

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,10 +3,11 @@ import thunk from 'redux-thunk';
 
 import AuthReducers from '../reducers/reducersAuth'; 
 import ProfileReducer  from '../reducers/reducersProfile';
+import ProductReducer  from '../reducers/reducersHome';
 
 const RootReducers = combineReducers({
     //reducers
-    AuthReducers, ProfileReducer
+    AuthReducers, ProfileReducer, ProductReducer
 });
 
 export default store = createStore(RootReducers, applyMiddleware(thunk));
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,19 +1,54 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { productData } from '../../redux/action/getProductData';
+import PagerView from 'react-native-pager-view';
+import { black } from '../../constant/index';
 
 const Home = () => {
 
+  const dispatch = useDispatch();
+
   const token = useSelector(state => state.AuthReducers.authToken);
-  console.log(token);
-  console.log(token.authToken);
+  const dataProduct = useSelector((state) => state.ProductReducer.dataProduct);
+
+  const [categoryId, setCategoryId] = useState(0);
+  const [querySearch, setQuerySearch] = useState('');
+  const [page, setPage] = useState(1);
+
+
+  useEffect(() => {
+    dispatch(productData({
+      // status: 'available',
+      // category_id: categoryId !== 0 ? categoryId : '',
+      // search: querySearch,
+      // page: page,
+    }))
+  }, [])
+
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+
+      <View>
+        <Text>{dataProduct.name}</Text>
+      </View>
+
     </View>
-  )
-}
+  );
+};
+
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+  },
+
+  name: {
+    color: black
+  }
+})
+
